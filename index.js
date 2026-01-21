711bcab222eed46fe5dac9d0723557b2f5978577
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-01-21 15:21:51 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const express = require('express');
 const app = express();
 
-const path = require('path');
+const path = require('path')
 
 const hbs = require('express-handlebars');
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts/'
-}));
+}))
+
 app.use(express.static('public'));
 
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "qwerty",
     database: "joga_mysql"
-});
+})
 
 con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to joga_mysql db");
+    if (err) throw err
+    console.log("Connected to joga_mysql db")
+})
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result;
+        res.render('index', { articles: articles })
+    })
+});
+
+app.listen(3000, () => {
+    console.log('App is started at http://localhost:3000');
 });
