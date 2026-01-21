8c31e8c82f612b5c9c784f6d7a88a34dd01e43df
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-01-21 13:18:14 +0200

    Added handlebars template engine

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const path = require('path');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
 const app = express();
 
+const path = require('path');
+
+const hbs = require('express-handlebars');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/'
+}));
+
+const mysql = require('mysql2');
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'qwerty',
-    database: 'joga_mysql'
-})
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql"
+});
 
-con.connect((err) => {
+con.connect(function(err) {
     if (err) throw err;
-    console.log('Connected to joga_mysql database');
-})
-
-app.listen(3003, () => {
-    console.log('App is started at http://localhost:3003');
-})
+    console.log("Connected to joga_mysql db");
+});
